1a647f9cb6cc6ecf152010b7358ebc12ef9a8327
jiangjiaxiong <1032030048@qq.com>
2018-08-31 16:11:16 +0800

    新增 vue-router 滚动行为。

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import {routerMode} from '@/config/env'
 import Router from 'vue-router'
-Vue.use(Router)
+
+Vue.use(Router);
 
 const index_user          = r => require.ensure([], () => r(require('index/pages/index/children/user')), 'index/index-user');
 const index_echart        = r => require.ensure([], () => r(require('index/pages/index/children/echart')), 'index/index-echart');
@@ -30,8 +31,8 @@ const routes = [
             path     : '',
             component: index_echart,
             meta     : [],
-        }]
-
+        }],
+        meta     : {scrollToTop: true}
     },
     {
         path     : '/index',
@@ -120,4 +121,27 @@ export default new Router({
     mode  : routerMode,
     routes,
     strict: process.env.NODE_ENV !== 'production',
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            // savedPosition is only available for popstate navigations.
+            return savedPosition
+        } else {
+            const position = {};
+            // new navigation.
+            // scroll to anchor by returning the selector
+            if (to.hash) {
+                position.selector = to.hash
+            }
+            // check if any matched route config has meta that requires scrolling to top
+            if (to.matched.some(m => m.meta.scrollToTop)) {
+                // cords will be used if no selector is provided,
+                // or if the selector didn't match any element.
+                position.x = 0;
+                position.y = 0;
+            }
+            // if the returned position is falsy or an empty object,
+            // will retain current scroll position.
+            return position
+        }
+    }
 })
