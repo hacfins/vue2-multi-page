d2e31f959d590af1a5d4603d096a12db79722a08
wangxinlai <568553494@qq.com>
2019-06-06 15:40:49 +0800

    修复POST请求 的问题

--- vue/src/config/ajax.js
+++ vue/src/config/ajax.js
@@ -1,6 +1,6 @@
 import {baseUrl} from './env'
 import axios from 'axios'
-import {IsPC, getCookie} from './utils';
+import {IsPC, getCookie,rootPath} from './utils';
 
 if (IsPC()) {
     var Message = require('element-ui').Message
@@ -102,25 +102,14 @@ export default function (url = '', options, type) {
     //补充为完整url
     url = baseUrl + url;
 
-    // 添加token
-    var hacfin_token = getCookie('MazeCloud');
-    if (hacfin_token) {
-        if (options.data) {
-            options.data['ACCESS-TOKEN'] = hacfin_token
-        } else {
-            options.data = {
-                'ACCESS-TOKEN': hacfin_token
-            }
-        }
-    }
-
     //  post 使用data作为参数，get使用params作为参数
     var config;
     if (type == 'POST') {
         config = {
             method: type,
             url   : url,
-            data  : options.data
+            data  : options.data,
+            withCredentials: true
         }
     } else {
         config = {
@@ -150,6 +139,81 @@ export default function (url = '', options, type) {
             case 404:
 
                 break;
+            case 458:
+                if (IsPC()) {
+                    Message.error(res.data.msg)
+                } else {
+                    Toast({
+                        message : res.data.msg,
+                        position: 'top'
+                    })
+                }
+                setTimeout(() => {
+                    if (IsPC()) {
+                        location.href = '/passport/login';
+                    } else {
+                        location.href = '/m/passport/login';
+                    }
+                },2000)
+                break;
+            case 403://缺少令牌
+            case 461://令牌无效
+            case 462://过期
+                if (IsPC()) {
+                    location.href = '/passport/login';
+                } else {
+                    location.href = '/m/passport/login';
+                }
+                break;
+            case 463://异地登录，被迫下线
+                if (IsPC()) {
+                    Message.error(res.data.msg)
+                } else {
+                    Toast({
+                        message : res.data.msg,
+                        position: 'top'
+                    })
+                }
+                setTimeout(() => {
+                    if (IsPC()) {
+                        location.href = '/passport/login';
+                    } else {
+                        location.href = '/m/passport/login';
+                    }
+                },2000)
+
+                break;
+            case 524://系统检测到有攻击行为存在
+                if (IsPC()) {
+                    Message.error(res.data.msg)
+                } else {
+                    Toast({
+                        message : res.data.msg,
+                        position: 'top'
+                    })
+                }
+                break;
+            case 706:
+                var reURL = document.location.href
+                if (reURL.indexOf("/product/auth") == -1)
+                    document.location.href = "/product/auth";
+                break;
+            case 711:
+                if (IsPC()) {
+                    Message.error(res.data.msg)
+                } else {
+                    Toast({
+                        message : res.data.msg,
+                        position: 'top'
+                    })
+                }
+                var reURL = document.location.href.substr(rootPath().length);
+                if (reURL.indexOf('/product/active') == -1)
+                    document.location.href = "/product/active";
+                break;
+            case 712:
+                document.location.href = "/";
+                break;
             default :
                 if (typeof (options["errorback"]) != 'undefined') {
                     options.errorback(res.data.msg, res.data.code, res.data.result);
@@ -175,10 +239,13 @@ export default function (url = '', options, type) {
             options.errorback(err)
         } else {
             if (IsPC()) {
-
                 Message.error(err)
             } else {
 
+                Toast({
+                    message : err,
+                    position: 'top'
+                })
             }
 
         }
