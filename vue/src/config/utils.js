0a9bf9953bcc2c3b18b2133f7a6edd960ccb0af9
wangxinlai <568553494@qq.com>
2019-10-18 17:58:41 +0800

    优化分享函数

--- vue/src/config/utils.js
+++ vue/src/config/utils.js
@@ -177,3 +177,134 @@ export const rootPath            = function () {
     var prePath     = strFullPath.substring(0, pos);
     return prePath;
 }
+
+function share_makeurl(name, data) {
+    var templates = {
+        qzone       : 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url={{URL}}&title={{TITLE}}&desc={{DESCRIPTION}}&summary={{SUMMARY}}&site={{SOURCE}}&pics={{IMAGE}}',
+        qq          : 'http://connect.qq.com/widget/shareqq/index.html?url={{URL}}&title={{TITLE}}&source={{SOURCE}}&desc={{DESCRIPTION}}&summary={{SUMMARY}}&site={{SOURCE}}&pics={{IMAGE}}',
+        tencent     : 'http://share.v.t.qq.com/index.php?c=share&a=index&title={{TITLE}}&url={{URL}}&pic={{IMAGE}}',
+        weibo       : 'http://service.weibo.com/share/share.php?url={{URL}}&title={{TITLE}}&pic={{IMAGE}}&appkey={{WEIBOKEY}}',
+        wechat      : '/wechat/share?url={{URL}}&title={{TITLE}}&summary={{SUMMARY}}&pics={{IMAGE}}',
+        douban      : 'http://shuo.douban.com/!service/share?href={{URL}}&name={{TITLE}}&text={{DESCRIPTION}}&image={{IMAGE}}&starid=0&aid=0&style=11',
+        diandian    : 'http://www.diandian.com/share?lo={{URL}}&ti={{TITLE}}&type=link',
+        linkedin    : 'http://www.linkedin.com/shareArticle?mini=true&ro=true&title={{TITLE}}&url={{URL}}&summary={{SUMMARY}}&source={{SOURCE}}&armin=armin',
+        facebook    : 'https://www.facebook.com/sharer/sharer.php?u={{URL}}&title={{TITLE}}&description={{DESCRIPTION}}&caption={{SUBHEAD}}&link={{URL}}&picture={{IMAGE}}',
+        twitter     : 'https://twitter.com/intent/tweet?text={{TITLE}}&url={{URL}}&via={{SITE_URL}}',
+        google      : 'https://plus.google.com/share?url={{URL}}'
+    };
+    if(data['summary'] == undefined){
+        data['summary'] = data['description'];
+    }
+    return templates[name].replace(/\{\{(\w)(\w*)\}\}/g, function (m, fix, key) {
+        var nameKey = name + fix + key.toLowerCase();
+        key = (fix + key).toLowerCase();
+
+        return encodeURIComponent((data[nameKey] === undefined ? data[key] : data[nameKey]) || '');
+    });
+}
+
+export const shareFn = function (el, opt) {
+    opt.summary     = opt.summary ? filterTitle(opt.summary).replace(/&nbsp;/gi, '').substr(0, 79) : '';
+
+    var reg_key_word = /公司+.*数据+.*/
+
+    if(reg_key_word.test( opt.summary )){
+
+        opt.summary = opt.summary.replace(/数据/gi,'**')
+    }
+    opt.title = opt.title.replace(/a/gi,'*')
+    opt.description = opt.description.replace(/a/gi,'*') + '（分享自 @公开课）';
+    var setdata     = {
+        site_url: location.origin,
+        source  : location.origin, // 来源（QQ空间会用到）, 默认读取head标签：<meta name="site" content="http://mooc.boolongo.com" />
+        sites   : ['weibo', 'wechat', 'qq', 'qzone'], // 启用的站点
+        disabled: ['google', 'facebook', 'twitter'], // 禁用的站点
+    };
+    setdata         = $.extend(setdata, opt)
+    if (IsPC()) {
+        //【1.0 PC】
+        socialShare(el, setdata);
+    } else {
+        if (!opt.url) {
+            setdata.url = location.href
+        }
+        var sharT = setdata.title;
+        el = typeof el === 'string' ? document.querySelectorAll(el) : el;
+        for(var i = 0; i < el.length; i++){
+            if(el[i].getAttribute('data-id') == 'weibo'){
+                setdata.title = setdata.description;
+            }else{
+                setdata.title = sharT;
+            }
+            if (el[i].getAttribute('data-id') == 'weibo' || el[i].getAttribute('data-id') == 'qzone') {
+                var $url = share_makeurl(el[i].getAttribute('data-id'), setdata);
+                el[i].setAttribute('href',$url)
+            }
+        }
+    }
+
+
+
+
+    //微信浏览器中，打开网页
+    if (/MicroMessenger/i.test(navigator.userAgent)) {
+
+        //签名
+        sdkSign({
+            data     : {'url': location.href},
+            callback : function (res) {
+
+                wx.config({
+                    debug    : false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                    appId    : res.appId, // 必填，公众号的唯一标识
+                    timestamp: res.timestamp, // 必填，生成签名的时间戳
+                    nonceStr : res.nonceStr, // 必填，生成签名的随机串
+                    signature: res.signature,// 必填，签名，见附录1
+                    jsApiList: res.jsApiList
+                });
+
+                var shareData = {
+                    title   : setdata.title,
+                    desc    : setdata.summary,
+                    link    : location.href,
+                    imgUrl  : setdata.image,
+                    trigger : function (res) {
+                    },
+                    complete: function (res) {
+                    },
+                    success : function (res) {
+                    },
+                    cancel  : function (res) {
+                    },
+                    fail    : function (res) {
+                    }
+                };
+
+                //监听微信分享
+                wx.ready(function () {
+                    // 2. 分享接口
+                    // 2.1 监听“分享到朋友圈”
+                    wx.onMenuShareTimeline(shareData);
+
+                    // 2.2 监听“分享给朋友”
+                    wx.onMenuShareAppMessage(shareData);
+
+                    // 2.3 监听“分享到QQ”
+                    wx.onMenuShareQQ(shareData);
+
+                    // 2.4 监听“分享到QQ空间”
+                    wx.onMenuShareQZone(shareData);
+
+                    // 2.4 监听“分享到微博”
+                    wx.onMenuShareWeibo(shareData);
+                });
+
+            },
+            errorback: function () {
+
+            }
+        });
+    }
+
+
+}
